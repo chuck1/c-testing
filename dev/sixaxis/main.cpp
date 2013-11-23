236f048f23c3d6dc09545f2a37b9c9db0b7cf40c
charles <charles@charles-HP-Pavilion-dv6000-RP283UA-ABA.(none)>
2013-11-22 23:08:50 -0800

    auto

---
+++ dev/sixaxis/main.cpp
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include <js/Master.h>
+#include <js/Event.h>
+
+int main(int argc,char** argv)
+{
+	js::master.Init();
+
+	
+	
+}
+
+
+
