6f33e1c6bdd9ca014189da2c5807a522c67e7640
Charles Rymal <charlesrymal@gmail.com>
2014-04-15 12:37:22 -0700

    permutation working

--- math/perm.cpp
+++ math/perm.cpp
@@ -1,46 +1,68 @@
 #include <cstdio>
 
+int	factorial(int n) {
+	if(n < 0) {
+		throw;
+	}
 
-void	permutation(int** perm, int* trans, int i, int n, int N) {
+	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+}
+void	permutation(int** perm, int* trans, int i, int N) {
 
-	printf("%i %i %i\n", i, n, N);
+	int n = factorial(N-i-1);
 
-	int* temp = *perm;
+	//printf("i=%i n=%i N=%i perm=%p\n", i, n, N, perm);
+	
+	int** temp = perm;
 	
 	if(i == N) {
-
+	
 	} else {
 		//for(int p = 0; p < n; ++p) {
 		int j = 0;
 		bool cont = false;
-		while(j < N) {
-			printf("j = %i\n",j);
+		for(int j = 0; j < N; j++) {
+			//printf("j = %i\n",j);
 			
 			cont = false;
 			for(int k = 0; k < i; k++) {
-				if(j == temp[k]) {
-					j++;
+				if(j == (*temp)[k]) {
 					cont = true;
+					//printf("j=%i taken\n",j);
 					break;
 				}
 			}
+			
 			if(cont) continue;
+			
+			//printf("j = %i\n",j);
+			
+			temp = perm;
 
-			printf("j = %i\n",j);
-
+			if(i+1 < N) {
+				//printf("perm = %p\n",perm);
+				//permutation(perm, trans, i+1, N);
+			}
+			
 			for(int p = 0; p < n; p++) {
-				printf("p = %i\n",p);
+				//printf("p = %i\n",p);
 				
+				//printf("perm = %p i = %i j = %i\n",perm,i,j);
+
 				(*perm)[i] = j;
 				(*trans)++;
 				
-				temp = *perm;
+				//temp = *perm;
 				
 				perm++;
 				trans++;
 			}
 			
-			j++;
+			if(i+1 < N) {
+				//printf("perm = %p\n",perm);
+				permutation(temp, trans, i+1, N);
+			}
+			
 		}
 		//}
 	}
@@ -51,20 +73,24 @@ int main() {
 	int** perm = new int*[6];
 	for(int i = 0; i < 6; i++) {
 		perm[i] = new int[3];
+		for(int j = 0; j < 3; j++) {
+			perm[i][j] = -1;
+		}
 	}
 	
 	
 	int trans[] = {0,0,0,0,0,0};
 	
-	permutation(perm, trans, 0, 2, 3);
+	permutation(perm, trans, 0, 3);
 
-	permutation(perm,   trans, 1, 1, 3);
-	permutation(perm+2, trans, 1, 1, 3);
-	permutation(perm+4, trans, 1, 1, 3);
-
+	//permutation(perm,   trans, 1, 1, 3);
+	//permutation(perm+2, trans, 1, 1, 3);
+	//permutation(perm+4, trans, 1, 3);
+	//permutation(perm+5, trans, 2, 3);
+	
 	for(int i = 0; i < 6; i++) {
 		for(int j = 0; j < 3; j++) {
-			printf("%i ",perm[i][j]);
+			printf("% i ",perm[i][j]);
 		}
 		printf("\n");
 	}
