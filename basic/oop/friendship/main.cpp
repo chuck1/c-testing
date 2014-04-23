6830be1dcf3183eda2d6dfbc94bc74c40cb804cc
Charles Rymal <charlesrymal@gmail.com>
2014-04-23 14:12:44 -0700

    custom class scope

--- basic/oop/friendship/main.cpp
+++ basic/oop/friendship/main.cpp
@@ -1,43 +1,109 @@
 
-class Helper;
+class Helper1;
+class Helper2;
 
-/* if you want a class or function to have access to only a subset
- * of the private members of your class, derive from a base class
- * that defines that subset of members and the friendship.
+class Constructor;
+class Derived;
+
+/* Creating custom class scopes
+ *
+ *       common
+ *      /  |   \
+ *     /   |    \
+ * base1  base2  ...
+ *    \    |    /
+ *     \   |   /
+ *      derived
+ * 
+ * common  - members used by all bases
+ * base    - members accessible by friends
+ * derived - constructor accessible by friends
+ * 
+ * the base classes contain member that are accessible only to its friends and derived
+ *
  */
 
-class Base {
-	public:
-		friend class Helper;
-
+class Common {
 	protected:
-		void fun1() {}
+		int data;
 };
 
-class Derived: public Base {
+class Base1: virtual public Common {
 	protected:
-		void fun2() {}
-
+		friend class Helper1;
+		Base1() {}
+	private:
+		void fun1() { data = 1; }
 };
 
-class Helper {
+class Base2: virtual public Common {
+	protected:
+		friend class Helper2;
+		Base2() {}
+	private:
+		void fun2() { data = 2; }
+};
+
+class Derived: public Base1, public Base2 {
+	protected:
+		friend class Constructor;
+		Derived() {}
 	public:
-		void fun(Derived& b) {
-			b.fun1();
-			b.fun2(); // error: ‘void Derived::fun2()’ is protected
+		void	fun() {
+			//Base1::fun1(); // error:
+			//Base2::fun2(); // error:
 		}
 };
 
+class Constructor {
+	public:
+		Derived*	create() {
+			return new Derived;
+		}
+};	
+
+class Helper1 {
+	public:
+		void fun1(Derived* d) {
+			d->fun1();
+			//d->fun2(); // error: ‘void Derived::fun2()’ is protected
+		}
+
+		Derived*	create() {
+			//return new Derived; // error: ‘Derived::Derived()’ is protected
+		}
+};
+class Helper2 {
+	public:
+		void fun2(Derived* d) {
+			//d->fun1(); // error: ‘void Derived::fun1()’ is protected
+			d->fun2();
+		}
+
+		Derived*	create() {
+			//return new Derived; // error: ‘Derived::Derived()’ is protected
+		}
+};
+
+
 int main() {
 
-	Derived d;
+	//Derived d; // error: ‘Derived::Derived()’ is protected
 	
-	Helper h;
+	Helper1 h1;
+	Helper2 h2;
+	Constructor c;
 
-	h.fun(d);
-	
-	
-	
+	Derived* d = c.create();
+
+	h1.fun1(d);
+	h2.fun2(d);
+
+	d->fun();
+
+	//d->fun1(); // error: ‘void Derived::fun2()’ is protected
+	//d->fun2(); // error: ‘void Derived::fun2()’ is protected
+
 }
 
 
