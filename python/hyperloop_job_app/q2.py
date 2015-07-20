fa0327dbcb3aacbefaa5fe480600df594f728f9d
chuck <charlesrymal@gmail.com>
2015-07-20 09:27:04 -0700

    finish hyperloop application

--- python/hyperloop_job_app/q2.py
+++ python/hyperloop_job_app/q2.py
@@ -17,14 +17,18 @@ T3 = 10
 D_inner = 6e-2
 D_outer = 10e-2
 mdot = 0.2
-q = 5e3
-
+q_outer = 5e3
 
 # assume circular
 
-L = (mdot * cp * (T1 - T3)) / (math.pi * D_outer * q)
+# heat per unit length
+q1 = math.pi * D_outer * q_outer
 
-h = q / (T2 - T1)
+q_inner = q_outer * D_outer / D_inner
+
+L = (mdot * cp * (T1 - T3)) / q1
+
+h = q_inner / (T2 - T1)
 
 A_cs = math.pi * (D_inner / 2)**2
 
@@ -35,14 +39,20 @@ Re_out = rho * v * D_inner / mu_out
 
 Pr_out = cp * mu_out / k
 
-Nu_D_out = 1.86 * (Re_out * Pr_out)**(1/3) * (D_inner / L)**(1/3) * (mu_out / mu_out_wall)**(0.14)
+#Nu_D_out = 1.86 * (Re_out * Pr_out)**(1/3) * (D_inner / L)**(1/3) * (mu_out / mu_out_wall)**(0.14)
+Nu_D_out = 4.36
 
-h2 = 
+# Nu=hD/k
 
-print "L    ",L
-print "h    ",h
-print "v    ",v
+h2 = Nu_D_out * k / D_inner
+
+print "q_outer ",q_outer
+print "q_inner ",q_inner
+print "L       ",L
+print "h       ",h
+print "v       ",v
 print "Re_in   ",Re_in
 print "Nu_D_out",Nu_D_out
+print "h2      ",h2
 
 
